0e14d4c870f3ee478ee3e5c0dc13ac75e007180a
fancmeng <1034406291@qq.com>
2017-03-20 16:02:42 +0800

    rally

--- rally/initialize.py
+++ rally/initialize.py
@@ -4,7 +4,7 @@ from mysql_tools.mysql_curd import *
 def initialize_mysql():
     tool = MysqlCurd('rally')
     tool.connect_mysql()
-    f = open('../sql', 'r')
+    f = open('sql', 'r')
     sql = ''
     for _ in f.readlines():
         sql += _.strip()
@@ -13,6 +13,3 @@ def initialize_mysql():
         tool.curor.execute(_)
         tool.conn.commit()
     tool.close_connect()
-
-if __name__ == '__main__':
-    initialize_mysql()
